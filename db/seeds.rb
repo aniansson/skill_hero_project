7926c61f38a71deabd2acd34daa55c08d6efa1ee
aniansson <ebba@aniansson.se>
2017-08-04 10:32:21 +0200

    Add users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([{
+                         email: "ebba@aniansson.se",
+                         first_name: "Ebba",
+                         last_name: "Aniansson"
+                     },
+                     {
+                         email: "hello@random.se",
+                         first_name: "Hello",
+                         last_name: "Random"
+                     },
+                     {
+                         email: "dino2@saur.se",
+                         first_name: "Dino",
+                         last_name: "Saur"
+                     },
+                     {
+                         email: "cookie@monster.se",
+                         first_name: "Cookie",
+                         last_name: "Monster"
+                     },
+                     {
+                         email: "captain@jacksparrow.se",
+                         first_name: "Jack",
+                         last_name: "Sparrow"
+                     }])
+
